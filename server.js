f51a135c8b7b0cac19de0a86396826ba85f54da8
Rulo180 <rullencio180@gmail.com>
2018-09-25 14:37:55 -0300

    - Add  dynamic response to root url in server.

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 'use strict';
 
 const express = require('express');
+const fetch = require('node-fetch');
 
 const PORT = 8080;
 const HOST = '127.0.0.1';
 
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello world\n');
+	const now = new Date();
+  res.send(`Hello, you have logged in at: ${now.toLocaleString()}`);
 });
 
 app.listen(PORT, HOST);
